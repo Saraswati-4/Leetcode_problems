bc72af7a35ed8641a59055e1145bd00ea386e75c
Saraswati <vermarajender183@gmail.com>
2026-07-06 12:16:34 +0530

    Time: 0 ms (100%), Space: 10.2 MB (4.61%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.c
@@ -0,0 +1,11 @@
+int maxProfit(int* prices, int pricesSize) {
+    int profit = 0;
+    int start = prices[0];
+    for(int i = 1;i<pricesSize; i++){
+        if(start < prices[i]){
+            profit += prices[i] - start;
+        }
+        start = prices[i];
+    }
+    return profit;
+}
